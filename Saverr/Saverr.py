b7979fa4455d4cff8d936328d799309b6fa8cddd
Roxedus <me@roxedus.dev>
2020-08-14 22:31:56 +0200

    [Saverr] Add logging

--- Saverr/Saverr.py
+++ Saverr/Saverr.py
@@ -3,6 +3,7 @@ import discord
 from redbot.core import checks, commands
 from redbot.core.bot import Red
 
+import logging
 import zipfile
 from io import BytesIO
 
@@ -14,6 +15,8 @@ class Saverr(commands.Cog):
 
     def __init__(self, bot: Red):
         self.bot = bot
+        self.log = logging.getLogger("red.roxcogs.saverr")
+        self.log.setLevel(logging.INFO)
 
     @checks.admin_or_permissions(manage_emojis=True)
     @commands.command()
@@ -22,9 +25,11 @@ class Saverr(commands.Cog):
         Saves the emojis to zip
         """
         mem_zip = BytesIO()
+        self.log.info("%s started saving emojis" % ctx.author.name)
         async with ctx.typing():
             with zipfile.ZipFile(mem_zip, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
                 for guild in self.bot.guilds:
+                    self.log.info("Saving emojis for %s" % guild.name)
                     for emoji in guild.emojis:
                         ext = "png"
                         if emoji.animated:
