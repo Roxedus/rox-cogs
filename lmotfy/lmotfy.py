455a7ce9776d40d4b4ac4b0ee5db4a0a0aa31339
Roxedus <me@roxedus.dev>
2022-04-23 02:39:13 +0200

    Switch to Lunr.py

--- lmotfy/lmotfy.py
+++ lmotfy/lmotfy.py
@@ -7,9 +7,11 @@ import asyncio
 import json
 import logging
 import urllib.request
+import urllib.parse
 from typing import Optional
 
-from fuzzywuzzy import fuzz
+import feedparser
+from lunr import lunr, index
 
 
 class Lmotfy(commands.Cog):
@@ -24,7 +26,9 @@ class Lmotfy(commands.Cog):
         self.bot.loop.create_task(cacher.loop())
 
         self._headers = {'User-Agent': 'HawkBot'}
-        self._orange = "https://theorangeone.net/index.json"
+        self._orange = "https://theorangeone.net"
+        self._orange_lunr = f"{self._orange}/search/index.json"
+        self._orange_rss = f"{self._orange}/posts/index.xml"
 
         self.log = logging.getLogger("red.roxcogs.lmotfy")
         self.log.setLevel(logging.INFO)
@@ -37,34 +41,53 @@ class Lmotfy(commands.Cog):
 
         async with ctx.typing():
 
-            scores = []
-            for index, item in enumerate(self._data):
-                values = list(item.values())
-                ratios = [fuzz.partial_ratio(str(words).lower(), str(value).lower()) for value in values]
-                scores.append({"index": index, "score": max(ratios)})
+            matches = self._idx.search(words)
 
-            filtered_scores = [item for item in scores if item['score'] >= 40]
-            sorted_filtered_scores = sorted(filtered_scores, key=lambda k: k['score'], reverse=True)
-            filtered_list_of_dicts = [self._data[item["index"]] for item in sorted_filtered_scores]
+            qString = urllib.parse.quote_plus(words)
 
             avatar = self.bot.user.avatar_url_as(format=None, static_format="png", size=1024)
-            embed = discord.Embed(color=0xF97C00)
+            embed = discord.Embed(color=0xF97C00, url=f"{self._orange}/search?q={qString}")
             embed.set_author(name=self.bot.user.name, icon_url=avatar)
+            embed.title = f"Here is the results of the search for `{words}`"
 
-            msg = ""
-            for i in range(7):
-                print(f"{sorted_filtered_scores[i]=}, {filtered_list_of_dicts[i]=}")
+            matchMap = {"score": 0, "count": 0}
 
-                msg += "[" + filtered_list_of_dicts[i]["title"] + "](" + filtered_list_of_dicts[i]["url"] + ")\n"
+            for match in matches:
+                matchMap["count"] += 1
+                matchMap["score"] = match["score"]
 
-            embed.description = msg
+                post = self.posts[match["ref"]]
 
-        await ctx.send(embed=embed)
+                embed.add_field(name=post['title'],
+                                value=f"{post['content'][:800]}\n\nScore: {match['score']}\n[Link]({post['link']})",
+                                inline=False)
+
+                if matchMap["count"] >= 7:
+                    break
+                if matchMap["score"] < 4:
+                    break
+
+            await ctx.send(embed=embed)
 
     def _get_index(self):
         try:
-            with urllib.request.urlopen(self._orange) as response:
-                self._data = json.load(response)
+
+            feed = feedparser.parse(self._orange_rss)
+            posts = {x["title"]: {"link": x["link"], "description": x["description"]} for x in feed.entries}
+
+            with urllib.request.urlopen(self._orange_lunr) as response:
+                data = json.loads(response.read().decode())
+                data = [i for i in data if posts.get(i["title"])]
+
+                idx = lunr(
+                    ref='id',
+                    fields=[dict(field_name='title', boost=10), 'content'],
+                    documents=data
+                )
+                self._idx = index.Index.load(idx.serialize())
+
+            self.posts = {x["id"]: {"content": posts[x["title"]]["description"], "title": x["title"],
+                                    "link": posts[x["title"]]["link"]} for x in data}
         except Exception:
             pass
 
